ec581710cfa7cb24a6f36877530402ccc44e768f
ZachJGreen <green.zach.j@gmail.com>
2026-02-12 19:27:00 -0600

    added __init__.py for cleaner access to classes

---
+++ View/__init__.py
@@ -0,0 +1,5 @@
+from View.main_window import MainWindow
+from View.counter_view import CounterView
+from View.game_grid import GameGrid
+from View.game_section import GameSection
+from View.menu_view import MenuView
